9ba5c617d4a393261b2d831741c48ca62758f1c6
Егор <en@welcomemedia.ru>
2021-07-05 18:07:17 +0300

    First commit

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, View, FlatList, Text } from "react-native";
+import { AddTodo } from "../components/AddTodo";
+import { Todo } from "../components/Todo";
+
+export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+  let content = (
+    <FlatList
+      keyExtractor={(item) => item.id.toString()}
+      data={todos}
+      renderItem={({ item }) => (
+        <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
+      )}
+    />
+  );
+
+  if (todos.length === 0) {
+    content = <View></View>
+  }
+
+  return (
+    <View>
+      <AddTodo onSubmit={addTodo} />
+
+      {content}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
