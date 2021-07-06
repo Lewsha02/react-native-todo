db7261e4492b24409f78a267744f9640c773a1de
Егор <en@welcomemedia.ru>
2021-07-06 11:36:46 +0300

    Add dimensions

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,23 +1,47 @@
-import React from "react";
-import { StyleSheet, View, FlatList, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList, Image, Dimensions } from "react-native";
 import { AddTodo } from "../components/AddTodo";
 import { Todo } from "../components/Todo";
+import { THEME } from "../theme";
 
 export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+  const [deviceWidth, setDeviceWidth] = useState(
+    Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2
+  );
+
+  useEffect(() => {
+    const update = () => {
+      const width =
+        Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2;
+      setDeviceWidth(width);
+    };
+
+    Dimensions.addEventListener("change", update);
+
+    return () => {
+      Dimensions.removeEventListener("change", update);
+    };
+  });
+
   let content = (
-    <FlatList
-      keyExtractor={(item) => item.id.toString()}
-      data={todos}
-      renderItem={({ item }) => (
-        <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
-      )}
-    />
+    <View style={{ width: deviceWidth }}>
+      <FlatList
+        keyExtractor={(item) => item.id.toString()}
+        data={todos}
+        renderItem={({ item }) => (
+          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
+        )}
+      />
+    </View>
   );
 
   if (todos.length === 0) {
     content = (
       <View style={styles.imgWrap}>
-        <Image style={styles.image} source={require("../../assets/no-items.png")} />
+        <Image
+          style={styles.image}
+          source={require("../../assets/no-items.png")}
+        />
       </View>
     );
   }
@@ -39,8 +63,8 @@ const styles = StyleSheet.create({
     height: 300,
   },
   image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'contain'
-  }
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
 });
