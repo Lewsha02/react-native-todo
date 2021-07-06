520e34ad6b408dd34d39d892c8ff17cb1994fa2f
Егор <en@welcomemedia.ru>
2021-07-06 18:40:56 +0300

    Add firebase to addTodo function

--- src/context/todo/todoState.js
+++ src/context/todo/todoState.js
@@ -1,18 +1,36 @@
 import React, { useReducer, useContext } from "react";
 import { Alert } from "react-native";
 import { ScreenContext } from "../screen/screenContext";
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types";
+import {
+  ADD_TODO,
+  CLEAR_ERROR,
+  HIDE_LOADER,
+  REMOVE_TODO,
+  SHOW_ERROR,
+  SHOW_LOADER,
+  UPDATE_TODO,
+} from "../types";
 import { TodoContext } from "./todoContext";
 import { todoReducer } from "./todoReducer";
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [{ id: "1", title: "Buy some water" }],
+    todos: [],
+    loading: false,
+    error: null,
   };
   const { changeScreen } = useContext(ScreenContext);
   const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  const addTodo = (title) => dispatch({ type: ADD_TODO, title });
+  const addTodo = async (title) => {
+    const response = await fetch("https://rn-todo-app-63eb4-default-rtdb.firebaseio.com/todos.json", {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ title })
+    });
+    const data = await response.json();
+    dispatch({ type: ADD_TODO, title, id: data.name });
+  };
 
   const removeTodo = (id) => {
     const todo = state.todos.find((t) => t.id === id);
@@ -33,6 +51,14 @@ export const TodoState = ({ children }) => {
   };
 
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+
+  const showLoader = () => dispatch({ type: SHOW_LOADER });
+
+  const hideLoader = () => dispatch({ type: HIDE_LOADER });
+
+  const showError = (error) => dispatch({ type: SHOW_ERROR, error });
+
+  const clearErorr = () => dispatch({ type: CLEAR_ERROR });
 
   return (
     <TodoContext.Provider
